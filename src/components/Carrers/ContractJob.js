58d1a463858f8aed8f92402a7d26958b9abfc56e
sahu69 <priyanka2022sahu@gmail.com>
2024-01-02 20:39:24 +0530

    Updates

--- src/components/Carrers/ContractJob.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ContractJob() {
-  return (
-    <div>ContractJob- component</div>
-  )
-}
--- src/components/Carrers/PermanentJob.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PermanentJob() {
-  return (
-    <div>PermanentJob component</div>
-  )
-}
